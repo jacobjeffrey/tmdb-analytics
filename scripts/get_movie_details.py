f7e590be6a3f1b240f472ec1e5e44e029fab22e9
jacobjeffrey <jeffreyaquino2@gmail.com>
2025-07-10 22:34:13 -0700

    Refactor get_movie_details and add utils

--- scripts/get_movie_details.py
+++ scripts/get_movie_details.py
@@ -1,7 +1,3 @@
-import os
-import time
-from pathlib import Path
-import json
 import csv
 
 from dotenv import load_dotenv
@@ -9,43 +5,33 @@ import pandas as pd
 import aiohttp
 import asyncio
 from aiolimiter import AsyncLimiter
-from tenacity import retry, stop_after_attempt, wait_exponential, retry_if_exception_type
+
+from utils import get_api_key, get_root_dir, ensure_path_exists, resolve_write_mode, chunked, fetch_api_data
 
 load_dotenv()
 
-current_file = Path(__file__).resolve()
-project_root = current_file.parents[1]
+project_root = get_root_dir()
 DATA_PATH = project_root / "data"
+ensure_path_exists(DATA_PATH)
 MOVIE_DETAILS_CSV = DATA_PATH / "movie_details.csv"
-os.makedirs(os.path.dirname(DATA_PATH), exist_ok=True) # ensure path exists
+movies_path = DATA_PATH / "movies.csv"
 BATCH_SIZE = 500 # how many ids to process before writing to csv
 
 # the movies.csv file needs to exist for this script to work
-movies_path = project_root / "data" / "movies.csv"
 try:
     df_movies = pd.read_csv(movies_path, engine="python")
     all_movie_ids = df_movies["id"]
 except:
-    print("Error, you must run get movies.csv before running this script.")
-    exit(1)
+    raise FileNotFoundError("You must run get_movies.py before running this script")
 
-# check if movie_details.csv exists
-if MOVIE_DETAILS_CSV.exists():
-    overwrite = input("movie_details.csv exists, overwrite? (y/n) ")
-    if overwrite.lower() == 'y':
-        write_mode = 'w'
-        movies_to_fetch = all_movie_ids
-    else:
-        write_mode = 'a'
-        movies_seen = pd.read_csv(MOVIE_DETAILS_CSV)["movie_id"]
-        movies_to_fetch = list(all_movie_ids.difference(movies_seen))
-else:
-    write_mode = 'w'
-    movies_to_fetch = all_movie_ids
+# check if movie_details.csv exists and if should overwrite or append
+options = resolve_write_mode(MOVIE_DETAILS_CSV, "id", all_movie_ids)
+write_mode = options["write_mode"]
+ids_to_fetch = options["ids_to_fetch"]
+header = options["header"]
 
-header = (write_mode == 'w')
-
-api_key = os.getenv("TMDB_API_KEY")
+# prepare API url and parameters
+api_key = get_api_key()
 BASE_URL = "https://api.themoviedb.org/3/movie/"
 params = {
     "api_key": api_key
@@ -55,48 +41,20 @@ params = {
 limiter = AsyncLimiter(max_rate=30, time_period=1)
 semaphore = asyncio.Semaphore(10)
 
-def notify_before_retry(retry_state):
-    print(f"Retrying movie {retry_state.args[1]}: attempt {retry_state.attempt_number}")
-
-# def batch function, to be put into utils later
-def chunked(iterable, batch_size):
-    try:
-        iterable = list(iterable)
-        for i in range(0, len(iterable), batch_size):
-            yield iterable[i:i + batch_size]
-    except:
-        raise TypeError("iterable could not be converted to list")
-
-@retry(
-        wait=wait_exponential(multiplier=1, min=1, max=10),
-        stop=stop_after_attempt(5),
-        retry=retry_if_exception_type((aiohttp.ClientError, asyncio.TimeoutError)),
-        before_sleep = notify_before_retry
-)
-async def fetch_movie_details(session, movie_id):
-    url = f"{BASE_URL}{movie_id}"
-    async with semaphore:
-        async with limiter:
-            async with session.get(url, params=params) as response:
-                try:
-                    response.raise_for_status()
-                    data = await response.json()
-                    for key, value in data.items():
-                        if isinstance(value, (list, dict)):
-                            data[key] = json.dumps(value)
-                except aiohttp.ClientResponseError as e:
-                    raise
-    return data
-
 async def collect_movie_details():
     write_mode = "w" if header else "a"  # ensure local scope
     local_header = header  # avoid mutation confusion
-    async with aiohttp.ClientSession() as session:
-        for batch in chunked(movies_to_fetch, BATCH_SIZE):
-            results = await asyncio.gather(
-                *[fetch_movie_details(session, id) for id in batch]
-            )
 
+    async with aiohttp.ClientSession() as session:
+        for i, batch in enumerate(chunked(ids_to_fetch, BATCH_SIZE)):
+            print(f"Fetching batch {i} of length {len(batch)}")
+            tasks = []
+            for id in batch:
+                url = BASE_URL + str(id)
+                tasks.append(fetch_api_data(url, session, params, semaphore, limiter))
+            results = await asyncio.gather(*tasks)
+
+            # write batch to csv
             df = pd.DataFrame(results)
             df.to_csv(MOVIE_DETAILS_CSV, mode = write_mode, index=False, header=local_header,
                     quoting=csv.QUOTE_ALL)
